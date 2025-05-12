3a3587c49d60bca287c83d445e67fa14dd67755e
Enrico Sabbadin <esabbadin@codearchitects.com>
2025-05-12 19:01:56 +0200

    wip

--- scripts/kubectl.sh
+++ scripts/kubectl.sh
@@ -3,7 +3,8 @@ helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 # Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart
 helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
  # open the dashboard port 
- kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
+ kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443 
+ https://localhost:8443
 # 
 
 # create service account for dashabord  
@@ -19,6 +20,9 @@ kubectl -n kubernetes-dashboard create token dashboard-user
 
 # install ngnix controller via helm 
  helm upgrade --install ingress-nginx ingress-nginx   --repo https://kubernetes.github.io/ingress-nginx   --namespace ingress-nginx --create-namespace
+helm upgrade --install  ingress-nginx ingress-nginx/ingress-nginx  --namespace ingress-nginx --set controller.ingressClassResource.name=nginx
+
+https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.2/deploy/static/provider/cloud/deploy.yaml
 
  ---------------
  
